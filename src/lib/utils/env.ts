bb27ef826937a489fe4dfc54f065d0222d125017
mkizka <g.mkizka@gmail.com>
2025-09-24 23:15:42 +0900

    エラーメッセージ修正

--- src/lib/utils/env.ts
+++ src/lib/utils/env.ts
@@ -2,11 +2,13 @@ import { isDid } from "@atproto/api";
 
 const asDidUrl = (value: string | undefined) => {
   if (!value) {
-    throw new Error("APPVIEW_DID_URL is not set");
+    throw new Error("NEXT_PUBLIC_APPVIEW_DID_URL is not set");
   }
   const [did, fragment] = value.split("#");
   if (!isDid(did) || !fragment) {
-    throw new Error(`APPVIEW_DID_URL is not a valid DID URL: ${value}`);
+    throw new Error(
+      `NEXT_PUBLIC_APPVIEW_DID_URL is not a valid DID URL: ${value}`
+    );
   }
   return `${did}#${fragment}` as const;
 };
